fd4844448e6edc6d8ff0387132358ae1b137dcea
om mistry <ommistry2914@gmail.com>
2026-03-18 23:18:02 +0530

    create entity,repo,dto for the user

---
+++ src/main/java/com/substring/auth/auth_app_backend/dtos/RoleDto.java
@@ -0,0 +1,18 @@
+package com.substring.auth.auth_app_backend.dtos;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDto {
+    private UUID id;
+    private String name;
+}
---
+++ src/main/java/com/substring/auth/auth_app_backend/dtos/UserDto.java
@@ -0,0 +1,25 @@
+package com.substring.auth.auth_app_backend.dtos;
+
+import com.substring.auth.auth_app_backend.entities.Provider;
+import com.substring.auth.auth_app_backend.entities.Role;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class UserDto {
+
+    private UUID id;
+    private String email;
+    private String name;
+    private String password;
+    private String image;
+    private boolean enable = true;
+    private Instant createdAt = Instant.now();
+    private Instant updatedAt =  Instant.now();
+    private Provider provider = Provider.LOCAL;
+    private Set<RoleDto> roles =  new HashSet<>();
+
+}
